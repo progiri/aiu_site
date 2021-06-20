234787555318997a1c9dda519c659d227492de76
[Yernar] <toktaryernar@gmail.com>
2021-06-21 01:42:40 +0600

    update

--- static/js/components/navbar.js
+++ static/js/components/navbar.js
@@ -1,3 +1,4 @@
+// Navbar
 let navbarCollapsed = false;
 
 $(".navbar-collpase-btn").click(() => {
@@ -14,18 +15,61 @@ $(".navbar-collpase-btn").click(() => {
   }
 });
 
+// Language
 let i18nListCollapsed = false;
 
 $(".navbar-i18n-btn").click(() => {
+  let currentLang =
+    window.localStorage.getItem("i18n") || "en-US";
+  let i18nListHTML = `
+  <div class="close-btn">
+      <div class="line"></div>
+      <div class="line"></div>
+    </div>`;
+
+  i18nLangList.forEach((el) => {
+    i18nListHTML += `<li data-lang-code="${
+      el.code
+    }" class='list-item ${
+      el.code === currentLang ? "list-item-active" : ""
+    }'>${el.text}</li>`;
+  });
+
+  $(".i18n-list").html(i18nListHTML);
+
   i18nListCollapsed = true;
   $(".i18n-list").addClass("i18n-list-collapsed");
 });
 
-$(".i18n-list .close-btn").click(() => {
-  i18nListCollapsed = false;
-  $(".i18n-list").removeClass("i18n-list-collapsed");
-});
+// Event delegation
+$("body").click(function (e) {
+  e = e || window.event;
+  let thisNode = e.target || e.srcElement;
 
-$(".i18n-list .list-item").click(function () {
-  console.log(this.dataset.langCode);
+  if ($.contains($(".i18n-list")[0], thisNode)) {
+    if (
+      $(thisNode).attr("class").indexOf("list-item") != -1
+    ) {
+      let { langCode: targetLang } = thisNode.dataset;
+      $("html").attr("lang", targetLang);
+      window.localStorage.setItem("i18n", targetLang);
+
+      Object.values($("html .i18n"))
+        .slice(0, Object.values($("html .i18n")).length - 2)
+        .forEach((el) => {
+          handleSwitchLanguage($(el)[0], targetLang);
+        });
+
+      i18nListCollapsed = false;
+      $(".i18n-list").removeClass("i18n-list-collapsed");
+    }
+
+    if (
+      $(thisNode).attr("class").indexOf("line") != -1 ||
+      $(thisNode).attr("class").indexOf("close-btn") != -1
+    ) {
+      i18nListCollapsed = false;
+      $(".i18n-list").removeClass("i18n-list-collapsed");
+    }
+  }
 });
