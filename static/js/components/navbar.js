630afaa0436e1c372212d6c68c9291d0118dc1f7
[Yernar] <toktaryernar@gmail.com>
2021-06-20 21:30:29 +0600

    add switch lang function

--- static/js/components/navbar.js
+++ static/js/components/navbar.js
@@ -13,3 +13,19 @@ $(".navbar-collpase-btn").click(() => {
     );
   }
 });
+
+let i18nListCollapsed = false;
+
+$(".navbar-i18n-btn").click(() => {
+  i18nListCollapsed = true;
+  $(".i18n-list").addClass("i18n-list-collapsed");
+});
+
+$(".i18n-list .close-btn").click(() => {
+  i18nListCollapsed = false;
+  $(".i18n-list").removeClass("i18n-list-collapsed");
+});
+
+$(".i18n-list .list-item").click(function () {
+  console.log(this.dataset.langCode);
+});
